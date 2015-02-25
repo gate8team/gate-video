278bb0d98350dc2aa6c7a3eb2804628d017d504a
ashabanov <me.antony.rain@gmail.com>
2015-02-26 02:59:14 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GateTv::Application.config.secret_key_base = '502565a6bff1d700db12d42fdc799abce4f8b39dbac2a039cb7433a6a024ec466e72a814de2a6f2845cfe32aef5f01a7f5599c85bd80779ab5f5458c9c6508c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GateTv::Application.config.session_store :cookie_store, key: '_gate-tv_session'
